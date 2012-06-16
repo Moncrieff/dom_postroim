a8243f05b5c4deca3ca685ab35f5cae1c918839c
Dmitry Veremchuk <dmitry.veremchuk@gmail.com>
2012-06-16 15:01:38 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DomPostroim::Application.config.secret_token = '5b7080e2f31477780a7afa1ab0b1428a2f28b5981fd1f5a9d2f35a111cc1f74fb0a34f32f35dafdbaa3709cfe3777af8780a868251a1787522e166b1efc5f4f0'
