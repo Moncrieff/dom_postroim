de1b4da439a16bbd0d391d2da9365e6293ff51a4
Dmitry Veremchuk <dmitry.veremchuk@gmail.com>
2012-07-05 07:49:44 +0400

    Nothing specific at all

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120701135420) do
+ActiveRecord::Schema.define(:version => 20120705031508) do
 
   create_table "jobs", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,29 @@ ActiveRecord::Schema.define(:version => 20120701135420) do
     t.string   "description"
     t.string   "location"
   end
+
+  create_table "tradesmen", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "username"
+    t.text     "about_me"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+  end
+
+  add_index "tradesmen", ["email"], :name => "index_tradesmen_on_email", :unique => true
+  add_index "tradesmen", ["reset_password_token"], :name => "index_tradesmen_on_reset_password_token", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
